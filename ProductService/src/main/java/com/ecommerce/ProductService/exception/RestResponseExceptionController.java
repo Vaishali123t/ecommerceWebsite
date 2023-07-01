7e86f8d0fb514c27d35753c99d8c7022f36543fc
Vaishali123t <tewarivaishali29@gmail.com>
2023-07-01 09:43:07 +0530

    overridden handleMethodArgumentNotValid in RestResponseExceptionController for handling MethodArgumentNotValidException due to validation error

--- ProductService/src/main/java/com/ecommerce/ProductService/exception/RestResponseExceptionController.java
+++ ProductService/src/main/java/com/ecommerce/ProductService/exception/RestResponseExceptionController.java
@@ -1,21 +1,50 @@
 package com.ecommerce.ProductService.exception;
 
 import com.ecommerce.ProductService.model.ErrorResponse;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class RestResponseExceptionController extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(ProductServiceException.class)
     private ResponseEntity<ErrorResponse> handleProductServiceException(ProductServiceException exception){
-            return new ResponseEntity<>(new ErrorResponse().builder()
-                    .errorMessage(exception.getMessage())
-                    .errorCode(exception.getErrorCode())
-                    .build(), HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(new ErrorResponse().builder()
+                .errorMessage(exception.getMessage())
+                .errorCode(exception.getErrorCode())
+                .build(), HttpStatus.NOT_FOUND);
     }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                           HttpHeaders headers, HttpStatus status,
+                                                                           WebRequest request){
+
+        Map<String, List<String>> body = new HashMap<>();
+
+        List<String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.toList());
+
+        body.put("errors", errors);
+
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    }
+
 
 }
